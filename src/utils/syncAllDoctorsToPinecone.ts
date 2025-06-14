defba7ef57eef401eb3d29ae7c9b28dae400a815
myself-sajed <shaikhsajedcoolpop@gmail.com>
2025-06-14 22:37:00 +0530

    enhance doctor synchronization by adding experience, gender, and consultation types to metadata

--- src/utils/syncAllDoctorsToPinecone.ts
+++ src/utils/syncAllDoctorsToPinecone.ts
@@ -8,29 +8,39 @@ export async function syncAllDoctorsToPinecone() {
     const vectors = await Promise.all(
         doctors.map(async (doc) => {
             const docText = `
-            ${doc.user.name} 
-            ${doc.specialization.join(", ")} 
-            ${doc.degree.join(", ")} 
-            ${doc.location?.address ?? ""}
-            ${doc.location?.city ?? ""}
-            ${doc.location?.state ?? ""}
-            ${doc.bio ?? ""}`;
+                ${doc.user.name}
+                ${doc.specialization.join(", ")}
+                ${doc.degree.join(", ")}
+                ${doc.experience} years experience
+                ${doc.bio ?? ""}
+                prefers ${doc.consultation_types ?? ""} consultation
+            `.toLowerCase(); // embedding text
 
             const embedding = await getVectorEmbeddings(docText);
+
             return {
                 id: doc._id.toString(),
                 values: embedding,
                 metadata: {
-                    userId: doc.user._id.toString(),
                     doctorId: doc._id.toString(),
-                    name: doc.user.name,
-                    specialization: doc.specialization,
+                    userId: doc.user._id.toString(),
+                    name: doc.user.name.toLowerCase(),
+                    specialization: doc.specialization.map((s) =>
+                        s.toLowerCase(),
+                    ),
+                    degrees: doc.degree.map((d) => d.toLowerCase()),
+                    gender: doc.user.gender?.toLowerCase(),
+                    languages: doc.languages.map((l) => l.toLowerCase()),
+                    consultation_types: doc.consultation_types?.toLowerCase(),
+                    city: doc.location?.city?.toLowerCase() ?? "",
+                    state: doc.location?.state?.toLowerCase() ?? "",
+                    country: doc.location?.country?.toLowerCase() ?? "",
+                    experience: doc.experience,
                 },
             };
         }),
     );
 
     await PineconeDoctorIndex.upsert(vectors);
-
     console.log(`✅ Synced ${vectors.length} doctors to Pinecone`);
 }
