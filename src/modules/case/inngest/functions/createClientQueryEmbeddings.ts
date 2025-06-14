ee411b027cdf92e1147226c354f86c49e21a6c0d
myself-sajed <shaikhsajedcoolpop@gmail.com>
2025-06-14 22:37:16 +0530

    add function to create client query embeddings from case data

---
+++ src/modules/case/inngest/functions/createClientQueryEmbeddings.ts
@@ -0,0 +1,16 @@
+import getVectorEmbeddings from "../../../../utils/getVectorEmbeddings";
+import { ICase } from "../../types/case-types";
+
+async function createClientQueryEmbeddings(c: ICase) {
+    const queryText = `
+    ${c.client_raw_query}
+    ${c.client_preferences.previous_conditions?.join(", ") ?? ""}
+    ${c.client_preferences.consultation_type ?? ""}
+    `;
+
+    const embeddings = await getVectorEmbeddings(queryText);
+
+    return embeddings;
+}
+
+export default createClientQueryEmbeddings;
