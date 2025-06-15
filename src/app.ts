c472e686217984af519488dab163b0d34d0a83c5
myself-sajed <shaikhsajedcoolpop@gmail.com>
2025-06-15 12:22:15 +0530

    add the inngest middleware to serve the flow

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import express, { NextFunction, Request, Response } from "express";
+import { serve } from "inngest/express";
 import { HttpError } from "http-errors";
 import bodyParser from "body-parser";
+import { inngest } from "./config/inngest";
+import onCaseCreation from "./modules/case/inngest/onCaseCreation";
 
 const app = express();
 app.use(bodyParser.json());
@@ -21,6 +24,15 @@ app.use("/api/v1/doctors", doctorRoutes);
 // case routes
 import caseRoutes from "./modules/case/routes/case-crud-routes";
 app.use("/api/v1/cases", caseRoutes);
+
+// serve inngest
+app.use(
+    "/api/inngest",
+    serve({
+        client: inngest,
+        functions: [onCaseCreation],
+    }) as express.RequestHandler,
+);
 
 // Global error handling
 app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
