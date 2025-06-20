9b45ec0c9f2fdb17b3b5b72cdf51ba5d616bd63b
myself-sajed <shaikhsajedcoolpop@gmail.com>
2025-06-21 00:37:56 +0530

    refactor createClientQueryEmbeddings to improve null safety for client preferences

--- src/modules/case/inngest/functions/createClientQueryEmbeddings.ts
+++ src/modules/case/inngest/functions/createClientQueryEmbeddings.ts
@@ -4,8 +4,8 @@ import { ICase } from "../../types/case-types";
 async function createClientQueryEmbeddings(c: ICase) {
     const queryText = `
     ${c.client_raw_query}
-    ${c.client_preferences.previous_conditions?.join(", ") ?? ""}
-    ${c.client_preferences.consultation_type ?? ""}
+    ${(c.client_preferences?.previous_conditions || [])?.join(", ") ?? ""}
+    ${c.client_preferences?.consultation_type ?? ""}
     `;
 
     const embeddings = await getVectorEmbeddings(queryText);
